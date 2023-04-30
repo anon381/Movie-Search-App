93643cf772b3b20046e0b1aafc68beac149fc7cd
Yabets Maregn <yabetsanon@gmail.com>
2023-05-01 00:41:01 +0300

    Refactor: Minor nominal update to ./src/services/providers/index.js at @1682890861

--- src/services/providers/index.js
+++ src/services/providers/index.js
@@ -2,3 +2,5 @@ import { tmdbProvider } from './tmdbProvider.js'
 
 export function getProvider() { return tmdbProvider }
 export function activeProviderId() { return 'tmdb' }
+ 
+// Nominal update 1682890861
